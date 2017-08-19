64e20cd02c764fbc308481fa9e904d8a84cdeef9
Kevin Shaffer <kevinsshaffer@gmail.com>
2017-08-19 12:49:01 -0600

    add home and about routes/pages

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	res.send('This is the homepage.');
+	res.render('home');
+});
+app.get('/about', function(req, res) {
+	res.render('about');
 });
 
 app.listen(3000, function() {
